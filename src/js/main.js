b52e5aa1538b0e0f46e0b92a308446a06adf9566
alvessh <alvesitamarsh@gmail.com>
2022-11-07 02:54:19 -0400

    end project

--- src/js/main.js
+++ src/js/main.js
@@ -22,11 +22,27 @@ function createGame(player1, hour, player2) {
 }
 
 document.querySelector("#cards").innerHTML =
-  createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "06:00", "cameroon") +
+      createGame("uruguay", "09:00", "southkorea") +
+      createGame("portugal", "12:00", "ghana") +
+      createGame("brazil", "15:00", "serbia")
+  ) +
   createCard(
     "28/11",
     "segunda",
-    createGame("switzerland", "13:00", "brazil") +
-      createGame("portugal", "16:00", "uruguay")
+    createGame("cameroon", "06:00", "serbia") +
+      createGame("southkorea", "09:00", "ghana") +
+      createGame("brazil", "12:00", "switzerland") +
+      createGame("portugal", "15:00", "uruguay")
   ) +
-  createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("southkorea", "11:00", "portugal") +
+      createGame("ghana", "11:00", "uruguay") +
+      createGame("serbia", "15:00", "switzerland") +
+      createGame("cameroon", "15:00", "brazil")
+  )
